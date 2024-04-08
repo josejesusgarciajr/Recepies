a00b090333f04890e70777f5afffbe71310da0be
josejesusgarciajr <37315121+josejesusgarciajr@users.noreply.github.com>
2024-04-08 12:24:17 -0700

    Cached Admin Credentials
    Cahced IsAdmin boolean value to not keep hitting the database

--- PersonalPlayGround/PersonalPlayGround/Controllers/BaseController.cs
+++ PersonalPlayGround/PersonalPlayGround/Controllers/BaseController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using PersonalPlayGround.ClientInfo;
 using PersonalPlayGround.ClientInfo.Repository;
+using System.Web.Caching;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System;
 
 namespace PersonalPlayGround.Controllers
 {
     public class BaseController : Controller
     {
         private IClientService _clientService;
+        private Cache runtimecache = HttpRuntime.Cache;
         public BaseController() { }
         public BaseController(IClientService clientService)
         {
@@ -16,6 +20,14 @@ namespace PersonalPlayGround.Controllers
         }
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            string username = User.Identity.Name;
+            string userSpecificCacheKey = "IsAdmin_" + username;
+            if (runtimecache.Get(userSpecificCacheKey) != null)
+            {
+                ViewBag.IsAdmin = Convert.ToBoolean(runtimecache.Get(userSpecificCacheKey));
+                return;
+            }
+
             bool isAdmin = false; // Logic to determine if the user is an admin
 
             if(_clientService == null)
@@ -23,7 +35,7 @@ namespace PersonalPlayGround.Controllers
                 _clientService = DependencyResolver.Current.GetService<IClientService>();
             }
 
-            Client client = _clientService.GetClientByUsername(User.Identity.Name);
+            Client client = _clientService.GetClientByUsername(username);
 
             if(client != null)
             {
@@ -31,6 +43,9 @@ namespace PersonalPlayGround.Controllers
             }
 
             ViewBag.IsAdmin = isAdmin;
+            runtimecache.Add(userSpecificCacheKey, client.IsAdmin, null, Cache.NoAbsoluteExpiration,
+                new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
+
             base.OnActionExecuting(filterContext);
         }
     }
--- PersonalPlayGround/PersonalPlayGround/Documents/AdminAuthorize.cs
+++ PersonalPlayGround/PersonalPlayGround/Documents/AdminAuthorize.cs
@@ -2,26 +2,38 @@
 using PersonalPlayGround.ClientInfo.Repository;
 using System;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace PersonalPlayGround.Documents
 {
     public class AdminAuthorize : AuthorizeAttribute
     {
+        private Cache runtimecache = HttpRuntime.Cache;
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             // get the current controller context
             var controllerContext = httpContext.Request.RequestContext.HttpContext;
-
-            // Check if the user is an admin
             var username = controllerContext.User.Identity.Name;
+
+            // check if cache exists
+            string userSpecificCacheKey = "IsAdmin_" + username;
+            if(runtimecache.Get(userSpecificCacheKey) != null)
+            {
+                return Convert.ToBoolean(runtimecache.Get(userSpecificCacheKey));
+            }
+
+            // no cache exists fetch information and cache it
             IClientService clientService = DependencyResolver.Current.GetService<IClientService>();
             Client client = clientService.GetClientByUsername(username);
 
-            if(client == null)
+            if (client == null)
             {
                 return false;
             }
+
+            runtimecache.Add(userSpecificCacheKey, client.IsAdmin, null, Cache.NoAbsoluteExpiration,
+                new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
 
             return client.IsAdmin;
         }
