fd02e9732f8391546df73669f6224a6eaa5e0424
josejesusgarciajr <37315121+josejesusgarciajr@users.noreply.github.com>
2024-11-17 19:17:27 -0800

    ErrorHandlerController for Custom statusCode Errors

---
+++ PersonalPlayGround/PersonalPlayGround/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PersonalPlayGround.Controllers
+{
+    public class ErrorHandlerController : Controller
+    {
+        public ActionResult PageNotFoundError()
+        {
+            return View();
+        }
+    }
+}
